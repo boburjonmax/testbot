5961ecca4752c9313c4a9beecb52658839fc7fd2
boburjonmax <bobur200009@mail.ru>
2025-11-07 10:07:45 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+﻿from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8016324927:AAFZhYMwHZGJD51FSBUXCCaylXeT9BhtsmA"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Salom do'stim', {update.effective_user.first_name}! Ahvolingiz qalay? 🙂")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot ishga tushdi...")
+    app.run_polling()
